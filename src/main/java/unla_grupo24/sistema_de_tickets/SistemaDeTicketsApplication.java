70735a203c8bdf94bde1aab1a434961aeda03602
Joel Fernandez <joelfernandez.relg@gmail.com>
2025-11-03 17:19:23 -0300

    Añado estructura básica del proyecto

---
+++ src/main/java/unla_grupo24/sistema_de_tickets/SistemaDeTicketsApplication.java
@@ -0,0 +1,13 @@
+package unla_grupo24.sistema_de_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unla_grupo24/sistema_de_tickets/SistemaDeTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package unla_grupo24.sistema_de_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
